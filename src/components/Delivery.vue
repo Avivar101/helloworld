4c3228932e243b28fc00508406f600eecebc4559
Avivar101 <63792174+Avivar101@users.noreply.github.com>
2020-08-17 15:49:44 +0100

    just adding components

---
+++ src/components/Delivery.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="info_block">
+        <h3>~ Delivery Info ~</h3>
+        <div class="info_block_content">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor repudiandae beatae quo deserunt numquam, assumenda quasi molestiae esse doloribus dolorem iure, pariatur necessitatibus rerum earum! Exercitationem consequuntur nulla natus possimus.</p>
+            <img src="../assets/images/delivery.png" alt="delivery Image">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'delivery'
+}
+</script>
---
+++ src/components/OrderingGuide.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="info_block">
+        <h3>~ How to Order ~</h3>
+        <div class="info_block_content">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor repudiandae beatae quo deserunt numquam, assumenda quasi molestiae esse doloribus dolorem iure, pariatur necessitatibus rerum earum! Exercitationem consequuntur nulla natus possimus.</p>
+            <img src="../assets/images/order.png" alt="order Image">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'orderingGuide'
+}
+</script>
